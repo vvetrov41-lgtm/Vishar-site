b952c4cba31a030fce605ad2571ab53d4e4bffc7
vvetrov41-lgtm <vvetrov41@gmail.com>
2026-03-01 08:28:56 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import(‘tailwindcss’).Config} */
 module.exports = {
-  content: ['./*.html', './index.html'],
-  theme: {
-    extend: {
-      colors: {
-        apple: {
-          black: '#000000',
-          darkGray: '#1d1d1f',
-          lightGray: '#f5f5f7',
-          blue: '#0071e3'
-        }
-      },
-      fontFamily: {
-        sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
-        serif: ['Playfair Display', 'serif']
-      }
-    }
-  },
-  plugins: []
+content: [
+“./index.html”,
+“./components.js”,
+“./colour-realism-tattoo-manchester/**/*.html”,
+“./black-and-grey-realism-manchester/**/*.html”,
+“./cover-up-tattoo-manchester/**/*.html”,
+“./about/**/*.html”,
+“./aftercare/**/*.html”,
+“./faq/**/*.html”,
+“./ai-tools/**/*.html”
+],
+theme: {
+extend: {
+colors: {
+apple: {
+black: ‘#000000’,
+darkGray: ‘#1d1d1f’,
+lightGray: ‘#f5f5f7’,
+blue: ‘#0071e3’
+}
+},
+fontFamily: {
+sans: [‘Inter’, ‘-apple-system’, ‘BlinkMacSystemFont’, ‘Segoe UI’, ‘sans-serif’],
+serif: [‘Playfair Display’, ‘Georgia’, ‘serif’]
+}
+}
+},
+plugins: []
 }
