8682044bae21c1d710a39dfca7f439e869335759
vvetrov41-lgtm <vvetrov41@gmail.com>
2026-08-04 17:34:08 +0100

    Set consultation shortcuts to 15-30 minutes

--- admin/src/pages/AppointmentsPage.tsx
+++ admin/src/pages/AppointmentsPage.tsx
@@ -32,8 +32,8 @@ const TYPES: AppointmentType[] = [
 
 const DURATION_MINUTES: Record<AppointmentType, number[]> = {
   tattoo_session: [180, 300, 420],
-  in_person_consultation: [30, 45, 60],
-  video_consultation: [20, 30, 45],
+  in_person_consultation: [15, 20, 30],
+  video_consultation: [15, 20, 30],
   touch_up: [60, 120, 180],
 };
 
