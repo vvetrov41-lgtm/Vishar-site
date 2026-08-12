7e21ea044c235cc40e039047e90438c134577617
vvetrov41-lgtm <vvetrov41@gmail.com>
2026-08-12 02:46:25 +0100

    Add narrow Monzo Supabase client

---
+++ workers/lib/monzo-supabase.js
@@ -0,0 +1,39 @@
+import { readSupabaseConfig } from './supabase.js';
+
+const ALLOWED_MONZO_RPCS = new Set([
+  'set_monzo_api_connection_status',
+  'register_monzo_reconciliation_candidate',
+]);
+
+export class MonzoSupabaseError extends Error {
+  constructor(status, code = 'monzo_database_request_failed') {
+    super(code);
+    this.name = 'MonzoSupabaseError';
+    this.status = status;
+    this.code = code;
+  }
+}
+
+export function createMonzoSupabaseClient(env, fetchImpl = fetch) {
+  const { url, authHeaders } = readSupabaseConfig(env);
+
+  async function rpc(name, args) {
+    if (!ALLOWED_MONZO_RPCS.has(name)) throw new Error('Monzo RPC is not allowed');
+    const response = await fetchImpl(`${url}/rest/v1/rpc/${name}`, {
+      method: 'POST',
+      headers: {
+        ...authHeaders,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(args ?? {}),
+    });
+    const body = await response.json().catch(() => null);
+    if (!response.ok) throw new MonzoSupabaseError(response.status);
+    return body;
+  }
+
+  return { rpc };
+}
+
+export const __testing = { ALLOWED_MONZO_RPCS };
